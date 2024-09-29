e975c92167de403b4b15435ff86e8ae4edf4bf9d
altie122 <77561831+altie122@users.noreply.github.com>
2024-09-29 00:53:52 -0600

    fix sitemap url

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'astro-test-7bu.pages.dev',
 	integrations: [mdx(), sitemap()],
 });
